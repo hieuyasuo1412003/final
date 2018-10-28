bb757e5910cba9e353a1a53276b04a66ed222b83
NguyenDuyHien065 <42117453+NguyenDuyHien065@users.noreply.github.com>
2018-10-28 14:14:37 +0700

    Update projectDare2.js

--- session-last-new/session-last-new-new/projectDare2.js
+++ session-last-new/session-last-new-new/projectDare2.js
@@ -3,25 +3,25 @@ var dareQuest = [
 
     "Scream my crush is ... and I want to do with her / him"
     "Kiss the lips of players of the opposite sex with you,"
-    "Perform three different 'caveats' with one player,"
+    "Perform three different 'sex style' with one player,"
     
-    "Take anything from any player,"
+    
     "Show a person selected by another player",
-    "Being hit by a single player in 10 shots,"
-    "Anti-Push 100",
+    "Being hit 10 times by a single player ",
+    "Push-up 100 times",
     "Performing a 'banana plantation' for 120 seconds,"
-    "Mustard in one minute,"
-    "For men, makeup, and women for makeup".
-    "Drop one designated item into the toilet and use the hand to remove it."
+    "Mustard in your mouth for one minute,",
+    "For men, makeup, and women clean the  makeup",
+    "Drop one  item into the toilet and use the hand to grab it.",
     "Fan dance for 1 minute",
     "Take off your pants for 5 minutes,"
-    "Lick the table"
-    "Two tablespoons of mustard"
+    "Lick the table",
+    "Two tablespoons of mustard",
     "Call Crush and say 'I love you'",
     "Kiss the cheek or lips of the same sex player",
     "For ice cold in the underpants",
     "Other players will edit your hairstyle",
-    "Sexy dance for 1 minute"
+    "Sexy dance for 1 minute",
     "Telling a joke that must make another player laugh,"
     "Call your opposite sex and confess,"
     
