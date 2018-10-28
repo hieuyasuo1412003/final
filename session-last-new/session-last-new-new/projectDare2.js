fac90818114df137b7f0193839032a0a1c465385
NguyenDuyHien065 <42117453+NguyenDuyHien065@users.noreply.github.com>
2018-10-28 14:15:14 +0700

    Update projectDare2.js

--- session-last-new/session-last-new-new/projectDare2.js
+++ session-last-new/session-last-new-new/projectDare2.js
@@ -1,7 +1,7 @@
 dare = document.querySelector(".clean-mtop-mbot")
 var dareQuest = [
 
-    "Scream my crush is ... and I want to do with her / him"
+    "Scream my crush is ... and I want to do with her / him",
     "Kiss the lips of players of the opposite sex with you,"
     "Perform three different 'sex style' with one player,"
     
@@ -9,8 +9,8 @@ var dareQuest = [
     "Show a person selected by another player",
     "Being hit 10 times by a single player ",
     "Push-up 100 times",
-    "Performing a 'banana plantation' for 120 seconds,"
-    "Mustard in your mouth for one minute,",
+    "Performing a 'banana plantation' for 120 seconds",
+    "Mustard in your mouth for one minute",
     "For men, makeup, and women clean the  makeup",
     "Drop one  item into the toilet and use the hand to grab it.",
     "Fan dance for 1 minute",
@@ -22,8 +22,8 @@ var dareQuest = [
     "For ice cold in the underpants",
     "Other players will edit your hairstyle",
     "Sexy dance for 1 minute",
-    "Telling a joke that must make another player laugh,"
-    "Call your opposite sex and confess,"
+    "Telling a joke that must make another player laugh",
+    "Call your opposite sex and confess",
     
 ];
 var choiceRandom = () => {
